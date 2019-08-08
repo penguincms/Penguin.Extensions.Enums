931073215658ae9ef2828d38579bbf00dba3e4c2
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:23:52 -0700

    Add files via upload

---
+++ EnumExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Penguin.Extensions.Enums
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public static class EnumExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    {
+        #region Methods
+
+        /// <summary>
+        /// Attempts to retrive all flag combinations from an enum
+        /// </summary>
+        /// <typeparam name="T">The enum to search</typeparam>
+        /// <returns> all flag combinations</returns>
+        public static List<T> GetFlags<T>() where T : Enum
+        {
+            List<T> flags = new List<T>();
+
+            foreach (T val in Enum.GetValues(typeof(T)))
+            {
+                ulong keyVal = System.Convert.ToUInt64(val);
+
+                if ((keyVal & (keyVal - 1)) == 0)
+                {
+                    flags.Add(val);
+                }
+            }
+
+            return flags;
+        }
+
+        #endregion Methods
+    }
+}
